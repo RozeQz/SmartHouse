77da87b042352299796d3cd7d54b1f42a24142f8
Lisa Rozhkova <liza.roz@yandex.ru>
2022-09-21 00:37:10 +0300

    Add measurements conversion to JSON

--- arduino_program/arduino_program.ino
+++ arduino_program/arduino_program.ino
@@ -1,32 +1,77 @@
 // библиотека для работы с датчиками серии DHT
 #include <TroykaDHT.h>
+#include <string.h>
+// пин подключения фоторезистора
+#define PHOTO_PIN A0
+
+class Data {
+  public:
+    int brightness;
+    float temperature;
+    float humidity;
+    char* getJSON();
+};
+
+class Station {
+  private:
+    char name[16];
+  public:
+    Data myData;
+    Station(const char* str) { strcpy(name, str); };
+    char* getJSON();
+};
+
+char* Data::getJSON() {
+  char* result = new char[64]();
+  strcat(result, "{ \"brightness\": ");
+  strcat(result, String(brightness).c_str());
+  strcat(result, ", \"temperature\": ");
+  strcat(result, String(temperature).c_str());
+  strcat(result, ", \"humidity\": ");
+  strcat(result, String(humidity).c_str());
+  strcat(result, "}");
+  return result;
+}
+
+char* Station::getJSON() {
+  char* result = new char[128]();
+  strcat(result, "{\"stationName\":\"");
+  strcat(result, name);
+  strcat(result, "\", \"data\":");
+  char* data = myData.getJSON();
+  strcat(result, data);
+  strcat(result, "}");
+  delete[] data;
+  return result;
+}
+
 // создаём объект класса DHT
 // передаём номер пина к которому подключён датчик и тип датчика
 // типы сенсоров: DHT11, DHT21, DHT22
 DHT dht(7, DHT11);
 
-void setup()
-{
+Station stn("Lisa's Oleg");
+
+void setup() {
+  pinMode(PHOTO_PIN, INPUT);
   // открываем последовательный порт для мониторинга действий в программе
   Serial.begin(9600);
   dht.begin();
 }
 
-void loop()
-{
+void loop() {
+  stn.myData.brightness = analogRead(PHOTO_PIN);
   // считывание данных с датчика
   dht.read();
   // проверяем состояние данных
-  switch(dht.getState()) {
+  switch (dht.getState()) {
     // всё OK
     case DHT_OK:
-      // выводим показания влажности и температуры
-      Serial.print("Temperature = ");
-      Serial.print(dht.getTemperatureC());
-      Serial.println(" C \t");
-      Serial.print("Humidity = ");
-      Serial.print(dht.getHumidity());
-      Serial.println(" %");
+      stn.myData.temperature = dht.getTemperatureC();
+      stn.myData.humidity = dht.getHumidity();
+      char* str = stn.getJSON();
+      Serial.println(str);
+      delete[] str;
       break;
     // ошибка контрольной суммы
     case DHT_ERROR_CHECKSUM:
@@ -41,7 +86,7 @@ void loop()
       Serial.println("Sensor not connected");
       break;
   }
-  
+
   // ждём две секунды
   delay(2000);
 }
