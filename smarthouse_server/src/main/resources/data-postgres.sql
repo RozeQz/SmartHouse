e039d8e7c7f858574c4192b1e6b9d3aea6373a46
Alexey Butyugin <Alex02072002@yandex.ru>
2022-11-11 14:00:48 +0300

    Modify index page

--- smarthouse_server/src/main/resources/data-postgres.sql
+++ smarthouse_server/src/main/resources/data-postgres.sql
@@ -1,5 +1,14 @@
-INSERT INTO Stations(stationID, stationName, temperature, brightness, humidity, error) VALUES(7, 'Test Station', 10, 20, 30, false);
-INSERT INTO Stations(stationID, stationName, temperature, brightness, humidity, error) VALUES(21, 'Another Test Station', 36.6, 120, 15, false);
-INSERT INTO Stations(stationID, stationName, temperature, brightness, humidity, error) VALUES(7, 'Test Station', 16, 150, 20, false);
-INSERT INTO Stations(stationID, stationName, temperature, brightness, humidity, error) VALUES(7, 'Test Station', 17, 130, 10, false);
-INSERT INTO Stations(stationID, stationName, temperature, brightness, humidity, error) VALUES(21, 'Another Test Station', 36.9, 130, 18, false);
+INSERT INTO Stations(stationID, stationName, temperature, brightness, humidity, error)
+VALUES
+    (7, 'Test Station', 10, 20, 30, false),
+    (21, 'Isabelle Station', 36.6, 120, 15, false),
+    (7, 'Test Station', 16, 150, 20, false),
+    (7, 'Test Station', 17, 130, 10, false),
+    (21, 'Isabelle Station', 36.9, 132, 20, false),
+    (21, 'Isabelle Station', 37.2, 136, 22, false),
+    (21, 'Isabelle Station', 35.2, 138, 17, false),
+    (90, 'Oleg Station', 20, 125, 30, false),
+    (90, 'Oleg Station', 26.6, 120, 15, false),
+    (90, 'Oleg Station', 23, 150, 20, false),
+    (91, 'Peter Station', 7, 30, 10, false),
+    (91, 'Peter Station', 6.9, 30, 18, false);
---
+++ smarthouse_server/src/main/resources/static/js/script.js
@@ -0,0 +1,20 @@
+window.addEventListener("load", setup_sliders);
+
+function setup_sliders() {
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-nav',
+        autoplay: true
+    });
+    $('.slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: false,
+        centerMode: true,
+        focusOnSelect: true
+    });
+}
