f8fb08bf57279915a1e92abf42487d9bd2bfe8ea
Lisa Rozhkova <liza.roz@yandex.ru>
2022-12-10 23:32:49 +0300

    Improve graph view

--- smarthouse_server/src/main/resources/static/js/chartjs.js
+++ smarthouse_server/src/main/resources/static/js/chartjs.js
@@ -1,52 +1,32 @@
 $(document).ready(function () {
-    var data_x = data.map(x => x["datetime"]);
+    var data_x = data.map(x => x["datetime"]).reverse();
     var data_temp = data.map(x => x["temperature"]);
     var data_br = data.map(x => x["brightness"]);
     var data_hum = data.map(x => x["humidity"]);
 
-    var lineChartTemp = {
+    var lineChart = {
         labels: data_x,
         datasets: [
             {
                 label: "Temperature",
-                fillColor: "rgba(220, 220, 220, 0.5)",
-                strokeColor: "rgba(220, 220, 220, 1)",
-                pointColor: "rgba(220, 220, 220, 1)",
-                pointStrokeColor: "#fff",
+                borderColor: "rgba(227, 153, 87, 1)",
+                backgroundColor: "rgba(227, 153, 87, 0.5)",
                 data: data_temp
             },
-        ]
-    };
-
-    var lineChartBr = {
-        labels: data_x,
-        datasets: [
             {
                 label: "Brightness",
-                fillColor: "rgba(220, 220, 220, 0.5)",
-                strokeColor: "rgba(220, 220, 220, 1)",
-                pointColor: "rgba(220, 220, 220, 1)",
-                pointStrokeColor: "#fff",
+                borderColor: "rgba(63, 173, 127, 1)",
+                backgroundColor: "rgba(63, 173, 127, 0.5)",
                 data: data_br
             },
-        ]
-    };
-
-    var lineChartHum = {
-        labels: data_x,
-        datasets: [
             {
                 label: "Humidity",
-                fillColor: "rgba(220, 220, 220, 0.5)",
-                strokeColor: "rgba(220, 220, 220, 1)",
-                pointColor: "rgba(220, 220, 220, 1)",
-                pointStrokeColor: "#fff",
+                borderColor: "rgba(123, 105, 150, 1)",
+                backgroundColor: "rgba(123, 105, 150, 0.5)",
                 data: data_hum
             },
         ]
     };
 
-    new Chart(document.getElementById("tempChart").getContext("2d"), {type: 'line', data: lineChartTemp });
-    new Chart(document.getElementById("brChart").getContext("2d"), {type: 'line', data: lineChartBr });
-    new Chart(document.getElementById("humChart").getContext("2d"), {type: 'line', data: lineChartHum });
+    new Chart(document.getElementById("lineChart").getContext("2d"), {type: 'line', data: lineChart });
 });
